4b8cfbaa8be63f73b756078b9d25de7e08d5209e
johnstonbrendan <brendan.johnston@uwaterloo.ca>
2019-11-18 09:44:17 -0500

    Try the two sensor setup. Fix parts of the code when testing is enabled.

--- JumpGameDetection/JumpGameDetection.ino
+++ JumpGameDetection/JumpGameDetection.ino
@@ -10,10 +10,9 @@
 #define NO_FOOT_DIST_2 100 //cm
 
 //below are definitions for testing
-#define TESTING_MODE 0
-#define NUMBER_OF_SENSORS 1 //only do 1 or 2
+#define TESTING_MODE 0 //0 for off 1 for on, turns on print statements
+#define NUMBER_OF_SENSORS 2 //only do 1 or 2
 
-long distance;
 FootDetector Detector_1 = FootDetector(TRIG_PIN_1,ECHO_PIN_1,FOOT_DIST_1,NO_FOOT_DIST_1);
 FootDetector Detector_2 = FootDetector(TRIG_PIN_2,ECHO_PIN_2,FOOT_DIST_2,NO_FOOT_DIST_2);
 
@@ -29,15 +28,17 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 #if TESTING_MODE
-  distance_1 = Detector_1.GetDistance();
-  Serial.print("Distance 1: "))
+  long distance_1 = Detector_1.GetDistance();
+  Serial.print("Distance 1: ");
   Serial.print(distance_1);
   Serial.println("cm");
-  Serial.print("Distance 2: "))
-  distance_2 = Detector_2.GetDistance();
+#if NUMBER_OF_SENSORS == 2
+  Serial.print("Distance 2: ");
+  long distance_2 = Detector_2.GetDistance();
   Serial.print(distance_2);
   Serial.println("cm");
   delay(60);
+#endif
   if(Detector_1.FootDetected()){
     Serial.println("Foot Detected (Detector 1)");
   }
@@ -51,6 +52,12 @@ void loop() {
   footDetected &= Detector_2.FootDetected();
 #endif
   dataForController_t controllerData = getBlankDataForController();
+#if TESTING_MODE
+  if(footDetected){
+    Serial.print("Foot Detected: ");
+    Serial.println(footDetected);
+  }
+#endif
   controllerData.dpadUpOn = footDetected;
   setControllerData(controllerData);
 #if NUMBER_OF_SENSORS == 2
